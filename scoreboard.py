3756d7a5a8acb897263e5f9131658b0e57da513a
gpapagapitos <george.papagapitos@slalom.com>
2024-07-05 10:02:29 -0500

    adding high score

--- scoreboard.py
+++ scoreboard.py
@@ -8,9 +8,11 @@ FONT = ("Courier", 24, "normal")
 
 class Scoreboard(Turtle):
     """Class representing a scoreboard"""
+
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -19,15 +21,22 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         """Update the scoreboard"""
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        """Display game over when the player loses"""
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        """Method to update the high score and reset the score"""
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     """Display game over when the player loses"""
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         """Increase the score by one and update the scoreboard"""
         self.score += 1
-        self.clear()
         self.update_scoreboard()
