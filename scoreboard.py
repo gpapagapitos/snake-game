80116b9bf668ab5f81d59d30eca20dbf98950c11
gpapagapitos <george.papagapitos@slalom.com>
2024-07-05 10:04:16 -0500

    refactor how high score is checked

--- scoreboard.py
+++ scoreboard.py
@@ -26,8 +26,7 @@ class Scoreboard(Turtle):
 
     def reset(self):
         """Method to update the high score and reset the score"""
-        if self.score > self.high_score:
-            self.high_score = self.score
+        self.high_score = max(self.high_score, self.score)
         self.score = 0
         self.update_scoreboard()
 
